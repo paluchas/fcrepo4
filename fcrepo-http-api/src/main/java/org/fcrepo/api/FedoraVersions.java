b5f4fe3cca49bac939c183c6e2e2108d4b04a151
fasseg <frank.asseg@googlemail.com>
2013-04-29 19:46:08 +0200

    added version resource and simple integration test

---
+++ fcrepo-http-api/src/main/java/org/fcrepo/api/FedoraVersions.java
@@ -0,0 +1,107 @@
+package org.fcrepo.api;
+
+import static org.slf4j.LoggerFactory.getLogger;
+
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+
+import javax.jcr.ItemNotFoundException;
+import javax.jcr.RepositoryException;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.PathSegment;
+import javax.ws.rs.core.Response;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlElementWrapper;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+import org.fcrepo.AbstractResource;
+import org.fcrepo.Datastream;
+import org.fcrepo.FedoraObject;
+import org.fcrepo.services.DatastreamService;
+import org.fcrepo.services.LowLevelStorageService;
+import org.fcrepo.services.ObjectService;
+import org.joda.time.DateTime;
+import org.slf4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+@Path("/rest/{path: .*}/fcr:versions")
+public class FedoraVersions extends AbstractResource{
+    private final Logger logger = getLogger(FedoraDatastreams.class);
+
+    @Autowired
+    private DatastreamService datastreamService;
+    
+    @Autowired
+    private ObjectService objectService;
+    
+    @GET
+    @Produces({MediaType.TEXT_XML, MediaType.APPLICATION_XML})
+    public List<Version> getVersionProfile(@PathParam("path") final List<PathSegment> segments) throws RepositoryException{
+    	final String path = toPath(segments);
+    	if (path.contains("/fcr:datastreams/")){
+    		Datastream ds = datastreamService.getDatastream(path);
+  			Version v = new Version(path, ds.getDsId(), ds.getCreatedDate().getTime());
+   			return Arrays.asList(v);
+    	}
+    	FedoraObject obj = objectService.getObject(path);
+    	Version v = new Version(path,obj.getName(),new Date().getTime());
+    	return Arrays.asList(v);
+    }
+
+    @Path("/{id}")
+    @GET
+    @Produces({MediaType.TEXT_XML, MediaType.APPLICATION_XML})
+    public Response getVersion(@PathParam("path") final List<PathSegment> segments, @PathParam("id") final String id) throws RepositoryException{
+    	final String path = toPath(segments);
+    	if (path.contains("/fcr:datastreams/")){
+    		Datastream ds = datastreamService.getDatastream(path);
+    		return Response.ok(ds).build();
+    	}
+    	FedoraObject obj = objectService.getObject(path);
+    	return Response.ok(obj).build();
+    }
+    
+    
+    
+    /* this is just a inner static to mock the yet non existant versioning model */
+    /* TODO: use a real versioning mechanism */
+    @XmlRootElement(name="version",namespace="http://example.com/test/version")
+    @XmlAccessorType(XmlAccessType.FIELD)
+    public static class Version {
+    	
+    	@XmlAttribute(name="path")
+    	private String path;
+    	@XmlAttribute(name="name")
+    	private String name;
+    	@XmlAttribute(name="timestamp")
+    	private long timestamp;
+    	
+    	private Version() {
+			super();
+		}
+
+    	public Version(String path, String versionId, long timestamp) {
+			this.name = versionId;
+			this.timestamp = timestamp;
+			this.path = path;
+		}
+		
+		public String getName() {
+			return name;
+		}
+		
+		public long getTimestamp() {
+			return timestamp;
+		}
+    }
+}
